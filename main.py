826fcd326f1baafabe04d14090cc64b07978fe50
Anatoly <anatolyskier@gmail.com>
2023-11-25 11:06:15 +0100

    very basic auth, a bit of refactoring

--- main.py
+++ main.py
@@ -3,78 +3,23 @@ import requests
 import os
 import uuid
 from dotenv import load_dotenv
-from flask import Flask, request, jsonify
+from flask import Flask, Request, request, jsonify
 from time import time
-
-
-GIGACHAT_API_URL = "https://gigachat.devices.sberbank.ru/api/v1/"
-GIGACHAT_TOKEN_URL = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
-load_dotenv()
-
-GIGACHAT_TOKEN = os.getenv("auth_data")
-AUTH_TOKEN = os.getenv("access_token")
-TOKEN_EXPIRATION = os.getenv("expires_at")
 
 APP_PORT = 5000
 APP_HOST = "0.0.0.0"
 
-def get_auth_token(auth_data: str):
-    headers = {
-        "Authorization": f"Bearer {auth_data}",
-        "RqUID": str(uuid.uuid4()),
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
-    data_urlencode = "scope=GIGACHAT_API_PERS"
-    response = requests.post(
-        GIGACHAT_TOKEN_URL, headers=headers, data=data_urlencode, verify=False
-    )
-    # TODO: save uuid and data of the request to db
-    print(f"code: {response.status_code}")
-    return response.json()
+GIGACHAT_API_URL = "https://gigachat.devices.sberbank.ru/api/v1/"
+GIGACHAT_TOKEN_URL = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
 
-def update_auth_token(auth_data: str | None | Any):
-    if auth_data is None:
-        return None, None
-    response = get_auth_token(auth_data)
-    return (response.get("access_token"), response.get("expires_at"))
+load_dotenv()
+
+PROMPTS = {
+    "loan_rating": "Сделай выжимку по данной истории. Оцени, насколько хорошая или плохая финансовая ситуация у человека и какие факторы на нее влияют: "
+}
 
 
-def ask_gigachat(auth_data: str, prompt: str):
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {auth_data}",
-    }
-
-    data = {
-        "model": "GigaChat:latest",
-        "messages": [{"role": "user", "content": prompt}],
-    }
-
-    response = requests.post(
-        GIGACHAT_API_URL + "chat/completions", headers=headers, json=data, verify=False
-    )
-    print(f"code: {response.status_code}")
-    return response.json()
-
-
-def get_models_gigachat(auth_data: str):
-    headers = {
-        "Authorization": f"Bearer {auth_data}",
-    }
-    response = requests.get(GIGACHAT_API_URL + "models", headers=headers, verify=False)
-    print(f"code: {response.status_code}")
-    return response.json()
-
-
-app = Flask(__name__)
-
-
-@app.route("/api/loan_rating", methods=["POST"])
-def loan_rating():
-    global TOKEN_EXPIRATION
-    global AUTH_TOKEN
-    history = """
-
+history = """
     Работа:
     1. Учитель, зарплата 20 т. р.
     2. Самозанятый, в среднем месячный доход 20 т. р.
@@ -126,51 +71,124 @@ def loan_rating():
      — Дата последнего платежа: 31.12.2022
     """
 
-    prompt = (
-        "Сделай выжимку по данной истории. Оцени, насколько хорошая или плохая финансовая ситуация у человека и какие факторы на нее влияют: "
-        + history
-    )
+app = Flask(__name__)
 
-    if  time() > int(TOKEN_EXPIRATION):
-        print(f"exp: {TOKEN_EXPIRATION}, {time()}")
-        AUTH_TOKEN, TOKEN_EXPIRATION = update_auth_token(GIGACHAT_TOKEN)
-        # #     # TODO: token update in runtime
-        # print("getting new reg token")
-        # new_token = get_auth_token(GIGACHAT_TOKEN)
-        # AUTH_TOKEN = new_token.get("access_token")
-        # TOKEN_EXPIRATION = new_token.get("expires_at")
+GIGACHAT_TOKEN = os.getenv("auth_data")
+
+if GIGACHAT_TOKEN is None:
+    print("Missing auth_data env variable")
+    exit()
+
+AUTH_TOKEN = os.getenv("access_token")
+if AUTH_TOKEN is None:
+    print("Missing access_token env variable")
+    exit()
+
+TOKEN_EXPIRATION = os.getenv("expires_at")
+if TOKEN_EXPIRATION is None:
+    print("Missing expires_at env variable, ignoring")
+    TOKEN_EXPIRATION = str(time())
+
+ACCESS_INTERNAL_TOKEN = os.getenv("access_token_internal")
+if ACCESS_INTERNAL_TOKEN is None:
+    print("No internal access token provided, some features may be disabled")
+
+
+def auth_internal(request: Request):
+    if request.headers.get("Authorization", "nope") != ACCESS_INTERNAL_TOKEN:
+        return False
+    return True
+
+
+def get_auth_token(auth_data):
+    app.logger.info("Getting auth token")
+    headers = {
+        "Authorization": f"Bearer {auth_data}",
+        "RqUID": str(uuid.uuid4()),
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+    data_urlencode = "scope=GIGACHAT_API_PERS"
+    response = requests.post(
+        GIGACHAT_TOKEN_URL, headers=headers, data=data_urlencode, verify=False
+    )
+    # TODO: save uuid and data of the request to db
+    app.logger.info(f"code: {response.status_code}")
+    return response.json()
+
+
+def update_auth_token(auth_data):
+    response = get_auth_token(auth_data)
+    return (response.get("access_token"), response.get("expires_at"))
+
+
+def ask_gigachat(auth_data: str, prompt: str):
+    app.logger.info(f"Asking gigachat {prompt}")
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {auth_data}",
+    }
+
+    data = {
+        "model": "GigaChat:latest",
+        "messages": [{"role": "user", "content": prompt}],
+    }
+
+    response = requests.post(
+        GIGACHAT_API_URL + "chat/completions", headers=headers, json=data, verify=False
+    )
+    app.logger.info(f"code: {response.status_code}")
+    return response.json()
+
+
+def get_models_gigachat(auth_data: str):
+    headers = {
+        "Authorization": f"Bearer {auth_data}",
+    }
+    response = requests.get(GIGACHAT_API_URL + "models", headers=headers, verify=False)
+    app.logger.info(f"code: {response.status_code}")
+    return response.json()
+
+
+@app.route("/api/loan_rating", methods=["POST"])
+def loan_rating():
+    global TOKEN_EXPIRATION
+    global AUTH_TOKEN
+
+    # TODO: get history from external service
+    global history
+
+    prompt = PROMPTS["loan_rating"] + history
+
+    if time() < int(TOKEN_EXPIRATION):
+        (AUTH_TOKEN, TOKEN_EXPIRATION) = update_auth_token(GIGACHAT_TOKEN)
 
     answer = ask_gigachat(AUTH_TOKEN, prompt)
-    print(answer)
+    app.logger.info(answer)
     # TODO: make it prettier
     response = {"answer": answer.get("choices")[0].get("message").get("content")}
     return jsonify(response)
 
 
 @app.route("/api/raw_prompt", methods=["POST"])
-def post_example():
+def raw_prompt():
     global TOKEN_EXPIRATION
     global AUTH_TOKEN
-    if request.method == "POST":
-        json_data = request.get_json()
-        prompt = json_data.get("prompt")
-        print(f"token expired: {time() > int(TOKEN_EXPIRATION)}")
-        if time() > int(TOKEN_EXPIRATION):
-            print(f"exp: {TOKEN_EXPIRATION}, {time()}")
-            #     # TODO: token update in runtime
-            print("getting new reg token")
-            new_token = get_auth_token(GIGACHAT_TOKEN)
-            AUTH_TOKEN = new_token.get("access_token")
-            TOKEN_EXPIRATION = new_token.get("expires_at")
 
-        answer = ask_gigachat(AUTH_TOKEN, prompt)
-        print(answer)
-        # TODO: make it prettier
-        response = {"answer": answer.get("choices")[0].get("message").get("content")}
-        # response = {"answer": "None"}
-        return jsonify(response)
-    else:
-        return jsonify({"status": "error", "message": "Invalid request method"})
+    json_data = request.get_json()
+    prompt = json_data.get("prompt")
+    if time() < int(TOKEN_EXPIRATION):
+        (AUTH_TOKEN, TOKEN_EXPIRATION) = update_auth_token(GIGACHAT_TOKEN)
+
+    answer = ask_gigachat(AUTH_TOKEN, prompt)
+    app.logger.info(answer)
+    # TODO: make it prettier
+    response = {"answer": answer.get("choices")[0].get("message").get("content")}
+    return jsonify(response)
+
+
+@app.route("/api/health_check", methods=["GET"])
+def health_check():
+    return jsonify({"status": "ok", "authenticated": auth_internal(request)})
 
 
 if __name__ == "__main__":
